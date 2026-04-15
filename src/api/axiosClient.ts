5b6df63b8facb2546e85b45a214062e23471b842
paneh-jpg <vtpanh39.work@gmail.com>
2026-04-15 22:58:44 +0700

    huhu

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAccessToken } from "../utils/token";
 
 const axiosClient = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
