fa41d9ef5d1f3e7a1d316fb32ed379b445fe6151
paneh-jpg <vtpanh39.work@gmail.com>
2026-04-15 22:51:02 +0700

    hehe

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Customers from "./pages/Customers";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/customers" element={<Customers />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/customers" element={<Customers />} />
+      </Routes>
+      <ToastContainer />
+    </>
   );
 }
 
